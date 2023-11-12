c059b6a7ed0150755128fd0bb90cc07b39cf1d07
khalidkakar12 <150319891+khalidkakar12@users.noreply.github.com>
2023-11-12 22:48:15 +0500

    first5

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <footer className="w-[100vw]  mt-5 flex flex-col md:flex-row items-center justify-center md:justify-between md:px-10 px-2">
       <div className='flex items-center flex-col md:flex-row space-x-3 justify-center '>
         <Link href={'/'} className="flex items-center space-x-4">
-        <Image src={'/profile.png'} width={38} height={38} alt='profile..' className='h-10 w-10 object-contain rounded-full' />
+        <Image src={'/profile.jpg'} width={38} height={38} alt='profile..' className='h-10 w-10 object-contain rounded-full' />
         <p className="font-bold text-lg text-yellow-400 font-Montserrat">Khalid&apos;s portfolio</p>
         </Link>
         <div className="border-2 border-l h-12 border-white hidden md:block"/>
